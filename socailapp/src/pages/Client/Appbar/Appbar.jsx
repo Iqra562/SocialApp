c58bfad613698626f0f53876f927bab32dafb576
Iqra Khaliq <iqra44938@gmail.com>
2024-07-24 22:14:53 -0700

    Update Appbar.jsx

--- socailapp/src/pages/Client/Appbar/Appbar.jsx
+++ socailapp/src/pages/Client/Appbar/Appbar.jsx
@@ -174,7 +174,7 @@ export default function PrimarySearchAppBar() {
             component="div"
             sx={{ display: { xs: 'none', sm: 'block' } }}
           >
-            MUI
+            MUw
           </Typography>
           <Search>
             <SearchIconWrapper>
