432c3a1cfd7884ac503c3ac4ba13fc4168fd423d
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2024-08-18 13:48:36 -0700

    responsive sidebar

--- socailapp/src/components/Client/SidebarCustom/SidebarCustom.jsx
+++ socailapp/src/components/Client/SidebarCustom/SidebarCustom.jsx
@@ -5,93 +5,125 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import { Avatar, Typography } from '@mui/material';
-import colors from '../../../ThemeProvider/color'
+import { Avatar, styled, Typography, useMediaQuery } from '@mui/material';
+import colors from '../../../ThemeProvider/color';
 import SideBarItems from './Sidebar';
 import { useContext } from 'react';
 import ThemeSwitcherContext from '../../../context/ThemeSwitcherContext/ThemeSwitcherContext';
-function SidebarCustom(){
+import { Link } from 'react-router-dom';
+
+function SidebarCustom() {
   const sideItems = SideBarItems();
-  const {mode} = useContext(ThemeSwitcherContext)
-    return (
-      <>
-        <Box  sx={{width:'15rem',height:'100vh', backgroundColor:colors[mode].background,   borderRight: `1px solid ${colors[mode].border}` }}>
-           <Box sx={{py:2}}>
+  const { mode } = useContext(ThemeSwitcherContext);
+  const isMdScreen = useMediaQuery((theme) => theme.breakpoints.down('md'));
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+  const StyledAvatar = styled(Avatar)(({ theme }) => ({
+    width: 20,
+    height: 20,
+    border: `1px solid ${colors[theme.palette.mode].background}`,
+    borderRadius: '100%',
+    boxShadow: `0 0 0 1px ${colors[theme.palette.mode].text}`,
+  }));
 
-      <Typography variant="h6" sx={{ px: 2.5,mt:2,mb:1,color:colors[mode].text }}>
-        Sidebar Heading
-      </Typography>
-           </Box>
-            <List sx={{px:0}}>
-              {sideItems.map((item,index)=>{
-              const IconComponent  = item.icon;
-              return(
+  const SideBox = styled(Box)(({ theme }) => ({
+    width: isSmallScreen ? '100%' : (isMdScreen ? '4rem' : '15rem'),
+    height: isSmallScreen ? 'auto' : '100vh',
+    backgroundColor: colors[theme.palette.mode].background,
+    borderRight: isSmallScreen ? 'none' : `1px solid ${colors[theme.palette.mode].border}`,
+    // display: 'flex',
+    flexDirection: isSmallScreen ? 'row' : 'column',
+    // alignItems: isSmallScreen ? 'center' : 'flex-start',
+    position:isSmallScreen ? 'fixed' :'static',
+    bottom: 0,
+    zIndex: 2000,
+    // overflow: isSmallScreen ? 'hidden' : 'hidden'
+  }));
 
-              
-              <ListItem key={index} disablePadding sx={{ display: 'block', py: 0.5, px: 1.5 }}>
-              <ListItemButton  sx={{
-                  minHeight:  20,
-                  justifyContent: 'initial' ,
-                  px:  1,
-                  py:  0.8,
-                  borderRadius: 2,
-                  
-                }}
-                onClick={item.onClick}
-                >
-                <ListItemIcon>  
-                  <IconComponent size='24' color={colors[mode].icon}/>  
+
+  const StyledListItemButton = styled(ListItemButton)(({ theme }) => ({
+    minHeight: 20,
+    justifyContent: 'initial',
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    paddingTop: theme.spacing(0.8),
+    paddingBottom: theme.spacing(0.8),
+    borderRadius: theme.shape.borderRadius * 2,
+  }));
+
+  const StyledListItemText = styled(ListItemText)(({ theme }) => ({
+    marginLeft: '-24px',
+    color: colors[theme.palette.mode].text,
+  }));
+
+  const StyledListItem = styled(ListItem)(({ theme }) => ({
+    display: 'block',
+    paddingTop: theme.spacing(0.5),
+    paddingBottom: theme.spacing(0.5),
+    paddingLeft:isSmallScreen ? 0 : theme.spacing(1.5),
+    paddingRight:isSmallScreen ? 0 : theme.spacing(1.5),
+    width: isSmallScreen ? 'auto' :'100%',
+    flexDirection: isSmallScreen ? 'row' : 'column',
+  }));
+
+  return (
+    <>
+      <SideBox>
+      {!isSmallScreen &&   <Box sx={{ py: 2 }}>
+     {   !isMdScreen ? (
+
+       
+       <Typography variant="h6" sx={{ px: 2.5, mt: 2, mb: 1, color: colors[mode].text }}>
+           Socail App 
+          </Typography>
+          ):(
+
+            <Typography variant="h6" sx={{ px: 2.5, mt: 2, mb: 1, color: colors[mode].text }}>
+            APP
+          </Typography>
+          )
+          }
+        </Box>}
+        <List sx={{ px: 0, display: 'flex',justifyContent:'space-around', flexDirection: isSmallScreen ? 'row' : 'column' }}>
+          {sideItems.map((item, index) => {
+            const IconComponent = item.icon;
+            const Content = (
+              <StyledListItemButton onClick={item.onClick}>
+                <ListItemIcon sx={{minWidth:isSmallScreen ? 0 : '56px'}}>
+                  <IconComponent size="24" color={colors[mode].icon} />
                 </ListItemIcon>
-                <ListItemText 
-  primary={item.name} 
-  sx={{ 
-    marginLeft: "-24px", 
-    fontWeight: 'bold', 
-    color: colors[mode].text 
-  }} 
-/>
+                {!isMdScreen && (
+                  <StyledListItemText primary={item.name} />
+                )}
+              </StyledListItemButton>
+            );
 
-              </ListItemButton> 
-            </ListItem>
-              )
-              })}
-            
-              <ListItem disablePadding sx={{ display: 'block', py: 0.5, px: 1.5 }}>
-                <ListItemButton  sx={{
-                    minHeight:  20,
-                    justifyContent: 'initial' ,
-                    px:  1,
-                    py:  0.8,
-                    borderRadius: 2,
-                    
-                  }}>
-                       <ListItemIcon sx={{}}>
+            return (
+              <StyledListItem key={index} disablePadding>
+                {item.Link ? (
+                  <Link to={item.Link} style={{ textDecoration: 'none' }}>
+                    {Content}
+                  </Link>
+                ) : (
+                  Content
+                )}
+              </StyledListItem>
+            );
+          })}
 
-                 <Avatar 
-      alt="User Name" 
-      src="/path/to/profile-image.jpg" 
-      sx={{ width: 20, height: 20 ,border:'1px solid white',borderRadius:'100%', boxShadow: `0 0 0 1px ${colors.light.text}`  }} 
-      />
-      </ListItemIcon>
-    <ListItemText primary="Inbox" sx={{ marginLeft: "-24px", color: colors[mode].text }} />
-                </ListItemButton> 
-              </ListItem>
-            
-            
-        
-          
-            </List>
-          
-         
-        </Box>
-      
-
-      </>
-
-      );
+          <StyledListItem disablePadding>
+            <StyledListItemButton>
+            <ListItemIcon sx={{minWidth:isSmallScreen ? 0 : '56px'}}>
+                <StyledAvatar alt="User Name" src="/path/to/profile-image.jpg" />
+              </ListItemIcon>
+              {!isMdScreen && (
+                <StyledListItemText primary="Inbox" />
+              )}
+            </StyledListItemButton>
+          </StyledListItem>
+        </List>
+      </SideBox>
+    </>
+  );
 }
 
 export default SidebarCustom;
