f2ad9c57d1409ebbe6862fb275a3644bbe8059b2
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2024-08-24 08:32:00 -0700

    reload user image

--- socailapp/src/components/Client/Card/Card.jsx
+++ socailapp/src/components/Client/Card/Card.jsx
@@ -20,6 +20,10 @@ import colors from '../../../ThemeProvider/color';
 import { useContext } from 'react';
 import { Box } from '@mui/material';
 import { icons } from '../../../utils/icons';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { firestore } from '../../../config/firebaseConfig';
  const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
@@ -31,29 +35,75 @@ import { icons } from '../../../utils/icons';
   }),
 }));
 
+
+
 export default function PostCard() {
+  const [posts, setPosts] = useState([]);
+const [loading, setLoading] = useState(false);
   const [expanded, setExpanded] = React.useState(false);
   const {mode} = useContext(ThemeSwitcherContext);
     const {comment :Comment,heartOutline:HeartOutline,bookmarkOutline:BookMark,} = icons
   const handleExpandClick = () => {
     setExpanded(!expanded); 
   };
-
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        //  Fetch all posts
+        const postsSnapshot = await getDocs(collection(firestore, "UserPosts"));
+  
+        const postsData = await Promise.all(
+          postsSnapshot.docs.map(async (postDoc) => {
+            const post = postDoc.data();
+            const userRef = doc(firestore, "Users", post.userId);
+            const userSnapshot = await getDoc(userRef);
+            
+            if (userSnapshot.exists()) {
+              const user = userSnapshot.data();
+              return {
+                ...post,
+                userName: user.username, 
+              };
+            } else {
+              return {
+                ...post,
+                userName: "Unknown User", 
+              };
+            }
+          })
+        );
+  
+        // Update state with fetched data
+        setPosts(postsData);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching posts or users:", error);
+        setLoading(false);
+      }
+    };
+  
+    fetchPosts();
+  }, []);
+  console.log(posts,"post")
   return (
+<>
+    {
+      posts.map((post,index)=>(
 <Card sx={{  marginBottom: "30px", boxShadow: 0, backgroundColor: colors[mode].background,
   borderBottom:`1px solid ${colors[mode].border}` ,borderRadius:0}}>
   <CardHeader
     avatar={
       <Avatar sx={{ bgcolor: red[500],width:'30px',height:'30px' }} aria-label="recipe">
-        R
+        {post.userName.split("")[0]}
       </Avatar>
     }
     sx={{ paddingX: 0, backgroundColor: colors[mode].background }}
-    title="Shrimp and Chorizo Paella"
+    title={post.userName}
   />
   <CardMedia
     component="img"
-    image={girl}
+    image={post.imageUrl
+    }
     alt="Paella dish"
     sx={{
       border: `1px solid ${colors[mode].border}`,
@@ -61,6 +111,7 @@ export default function PostCard() {
       borderRadius:'2px',
       boxSizing: 'border-box', 
     }}
+  
   />
   <CardActions disableSpacing sx={{ paddingX: 0, backgroundColor: colors[mode].background, display: 'flex', justifyContent: 'space-between' }}>
     <Box>
@@ -79,10 +130,14 @@ export default function PostCard() {
   </CardActions>
   <CardContent sx={{ padding: 0, backgroundColor: colors[mode].background }}>
     <Typography variant="body2" color="text.secondary">
-      This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.
+     {post.title}
     </Typography>
   </CardContent>
 </Card>
+
+      ))
+    }
+    </>
 
   );
 }
